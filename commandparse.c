95c7c8f5ff07ab1c3530e2a164d842efe15dddb6
Michelle <mnikki137@gmail.com>
2014-02-03 21:44:15 -0700

    Initial commit of commandparse.

---
+++ commandparse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+ int main (int argc, char *argv[]) {
+	 int i =0;
+	 printf("\ncmdline args count =%d\n", argc);
+
+ 	 printf("\nexe name=%s", argv[0]);
+
+	 for (i=1; i< argc; i++) {
+		 printf("\narg%d=%s", i, argv[i]);
+	 }
+	 printf("\n");
+	 return 0;
+ }
+
