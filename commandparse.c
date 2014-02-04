24fdf0bba2fbd9d172f2b6dc72be53d085517fe8
Michelle <mnikki137@gmail.com>
2014-02-03 23:24:30 -0700

    Validated that path points to valid director.

--- commandparse.c
+++ commandparse.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdbool.h>
+
+bool isDir(const char* dirpath)
+{
+	struct stat dirstat;
+	stat(dirpath, &dirstat);
+	return S_ISDIR(dirstat.st_mode);
+}
 
 int main (int argc, char *argv[]) {
 	char *dirpath = 0;
+	int result = 0;
 
 	if (argc != 2) { 
 		printf("Try again asshole.\n");
 		exit(1); 
 	}
 	dirpath = argv[1];
-	printf("Your directory path is [%s]\n", dirpath);
+	printf("Your directory path is [%s]\n", dirpath); /* XXX */
+	
+	if (isDir(dirpath) == 0){
+		printf("Please enter a valid path\n");
+		exit(2);
+	}
 }
 
 
